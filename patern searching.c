4c1a7fbfe9a8677bfd2d7b2c3591e91d40f710e2
Pantchanit555 <50312606+Pantchanit555@users.noreply.github.com>
2020-02-04 18:37:39 +0700

    Add files via upload

---
+++ patern searching.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<string.h>
+main(){
+	char wd[100],wdCheck[100];
+	scanf("%s",wd);
+	scanf("%s",wdCheck);
+	int i,j,n=0;
+	for(i=0;i<strlen(wd)-strlen(wdCheck)+1;i++){
+		for(j=0;j<strlen(wdCheck);j++){
+			if(wd[i+j] != wdCheck[j]){
+				break;
+			}
+		}
+		if(j==strlen(wdCheck)){
+			n++;
+			printf("%d\n",i);
+		}
+	}
+	printf("%d",n);
+}
